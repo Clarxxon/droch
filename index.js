64e49b7195571eb5130b7178cabf84be04fd3213
Alexei Tyurin <leha2148@gmail.com>
2018-11-09 17:38:20 +0300

    start

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const fs = require('fs'),
+    path = require('path'),
+    http = require('http');
+
+const process = require('process');
+const appConnect = require('connect')();
+const swaggerTools = require('swagger-tools');
+const jsyaml = require('js-yaml');
+const serverPort = process.env.PORT || 3000;
+
+const Knex = require('knex');
+const yaml_conf = require('yaml-config');
+const settings = yaml_conf.readConfig('./app.yaml', 'env_variables');
+
+// swaggerRouter configuration
+const options = {
+    swaggerUi: path.join(__dirname, '/swagger.json'),
+    controllers: path.join(__dirname, './controllers'),
+    useStubs: process.env.NODE_ENV === 'production' // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+const spec = fs.readFileSync(path.join(__dirname, 'api/swagger.yaml'), 'utf8');
+const swaggerDoc = jsyaml.safeLoad(spec);
+
+// Подключаемся к БД
+module.exports.knex = connect();
+
+function connect() {
+    // [START gae_flex_postgres_connect]
+    const config = {
+        user: process.env.SQL_USER || settings.SQL_USER,
+        password: process.env.SQL_PASSWORD || settings.SQL_PASSWORD,
+        database: process.env.SQL_DATABASE || settings.SQL_DATABASE,
+    };
+
+    if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+        config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+    }
+
+    // Данные подключения к БД
+    return Knex({
+        client: 'pg',
+        connection: config
+    });
+}
+
+appConnect.use(function (req, res, next) {
+
+// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+// Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+// res.setHeader('Access-Control-Allow-Credentials', true);
+
+// Pass to next layer of middleware
+    next();
+});
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    appConnect.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    appConnect.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    appConnect.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    appConnect.use(middleware.swaggerUi());
+
+    // Start the server
+    http.createServer(appConnect).listen(serverPort, function () {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    });
+
+});
---
+++ service/ClientService.js
@@ -0,0 +1,272 @@
+'use strict';
+
+const MethodDB = require('../db_method/MethodClient');
+const knex = require('../index').knex;
+const webshot = require('webshot');
+const uniqid = require('uniqid');
+
+/**
+ * Создание диалога (НЕ привязанного к заявке) клиентом
+ *
+ * cli_id Integer ID клиента
+ * returns inline_response_200_8
+ **/
+exports.clientSetDialog = function (cli_id) {
+
+    const TAG = "clientSetDialog";
+
+    const this_dialog = {
+        cli_id: cli_id,
+        dlg_begindt: new Date()
+    };
+
+    return new Promise(function (resolve) {
+        const result = {};
+        result['application/json'] = {
+            "dlg_id": null,
+            "status": "SERVER ERROR"
+        };
+
+        MethodDB.insertDialog(knex, this_dialog)
+            .then((res) => {
+                console.log(TAG + " -> result: good");
+                result['application/json'] = {
+                    "dlg_id": res[0],
+                    "status": "OK"
+                };
+            })
+            .catch((err) => {
+                console.error(TAG + " -> result: " + err);
+                result['application/json'] = {
+                    "dlg_id": null,
+                    "status": "ERROR"
+                };
+            })
+            .finally(() => {
+                resolve(result[Object.keys(result)[0]]);
+            });
+    });
+};
+
+
+/**
+ * Создать клиента и получить его ID
+ *
+ *
+ * body Client Данные клиента
+ * returns inline_response_200_11
+ **/
+exports.getClient = function (body, IP) {
+    const TAG = "getClient";
+
+    // Создание записи посещения для хранения в БД
+    const this_cilent =
+        {
+            cli_ip: IP,
+            cli_fullname: body.cli_fullname,
+            cli_email: body.cli_email,
+            cli_phone: body.cli_phone,
+            cli_address: body.cli_address,
+            cli_passeries: body.cli_passeries,
+            cli_pasnumber: body.cli_pasnumber,
+            cli_pasissued: body.cli_pasissued,
+            cli_pasdateissued: body.cli_pasdateissued,
+            cli_pasdeviscode: body.cli_pasdeviscode
+        };
+
+    return new Promise(function (resolve, reject) {
+        const result = {};
+        result['application/json'] = {
+            "cli_id": null,
+            "status": "SERVER ERROR"
+        };
+
+        try {
+            MethodDB.insertClient(knex, this_cilent)
+                .then((res) => {
+                    console.log(TAG + " -> result: good");
+                    result['application/json'] = {
+                        "cli_id": res[0],
+                        "status": "OK"
+                    };
+                })
+                .catch((err) => {
+                    console.error(TAG + " -> result: " + err);
+                    result['application/json'] = {
+                        "cli_id": null,
+                        "status": "ERROR"
+                    };
+                })
+                .finally(() => {
+                    resolve(result[Object.keys(result)[0]]);
+                });
+        }
+        catch (err) {
+            result['application/json'] = {
+                "cli_id": null,
+                "status": "SERVER ERROR"
+            };
+            console.error(TAG + " -> result: " + err);
+            reject(result[Object.keys(result)[0]]);
+        }
+    });
+};
+
+
+/**
+ * Получить скриншот от сервера
+ *
+ *
+ * uRL String URL ресурса, скриншот которого необходимо получить
+ * returns inline_response_200_10
+ **/
+exports.getScreen = function (uRL, host) {
+    const TAG = "getScreen";
+
+    const options = {
+        streamType: 'png',
+        windowSize: {
+            width: 1920,
+            height: 1080
+        },
+        shotSize: {
+            width: 'all',
+            height: 1080
+        }
+    };
+
+    const imgName = uniqid();
+
+    const result = {};
+    result['application/json'] = {
+        "imgsource": null,
+        "status": "SERVER ERROR"
+    };
+
+    return new Promise(function (resolve) {
+        webshot(uRL, "./public/" + imgName + "." + options.streamType, options, (err) => {
+            if (err) {
+                console.error(TAG + " -> result: " + err);
+                result['application/json'] = {
+                    "imgsource": null,
+                    "status": "ERROR"
+                };
+
+                resolve(result[Object.keys(result)[0]]);
+            }
+            console.log(TAG + " -> result: good");
+            result['application/json'] = {
+                "imgsource": host + "/public/" + imgName + "." + options.streamType,
+                "status": "OK"
+            };
+            resolve(result[Object.keys(result)[0]]);
+        });
+    });
+
+};
+
+
+/**
+ * Создание диалога, привязанного к заявке
+ *
+ * body Body_8 ID запроса и ID клиента
+ * returns inline_response_200_8
+ **/
+exports.setDialogForRqt = function (body) {
+    const TAG = "setDialogForRqt";
+
+    const this_dialog = {
+        cli_id: body.cli_id,
+        dlg_begindt: new Date()
+    };
+
+    return new Promise(function (resolve) {
+        const result = {};
+        result['application/json'] = {
+            "dlg_id": null,
+            "status": "SERVER ERROR"
+        };
+
+        MethodDB.insertDialog(knex, this_dialog)
+            .then((res) => MethodDB.updRequestDlg(knex, res[0], body.rqt_id))
+            .then((res) => {
+                if(res.length === 0) throw new Error("Invalid update parameter dlg_id for request");
+                console.log(TAG + " -> result: good");
+                result['application/json'] = {
+                    "dlg_id": res[0],
+                    "status": "OK"
+                };
+            })
+            .catch((err) => {
+                console.error(TAG + " -> result: " + err);
+                result['application/json'] = {
+                    "dlg_id": null,
+                    "status": "ERROR"
+                };
+            })
+            .finally(() => {
+                resolve(result[Object.keys(result)[0]]);
+            });
+    });
+};
+
+
+/**
+ * Создать заявку по ID клиента
+ *
+ *
+ * body Request Данные заявки
+ * returns inline_response_200_12
+ **/
+exports.setRequest = function (body) {
+    const TAG = "setRequest";
+
+    const this_request =
+        {
+            cli_id: body.cli_id,
+            rqt_url: body.rqt_url,
+            rqt_comment: body.rqt_comment,
+            rqt_imgsource: body.rqt_imgsource,
+            rqt_dt: new Date(),
+            rqt_blockedstatus: 0,
+            rqt_status: 0
+        };
+
+    return new Promise(function (resolve, reject) {
+        const result = {};
+        result['application/json'] = {
+            "rqt_id": null,
+            "status": "SERVER ERROR"
+        };
+
+        try {
+            MethodDB.insertRequest(knex, this_request)
+                .then((res) => {
+                    console.log(TAG + " -> result: good");
+                    result['application/json'] = {
+                        "rqt_id": res[0],
+                        "status": "OK"
+                    };
+                })
+                .catch((err) => {
+                    console.error(TAG + " -> result: " + err);
+                    result['application/json'] = {
+                        "rqt_id": null,
+                        "status": "ERROR"
+                    };
+                })
+                .finally(() => {
+                    resolve(result[Object.keys(result)[0]]);
+                });
+        }
+        catch (err) {
+            result['application/json'] = {
+                "rqt_id": null,
+                "status": "SERVER ERROR"
+            };
+            console.error(TAG + " -> result: " + err);
+            reject(result[Object.keys(result)[0]]);
+        }
+    });
+};
+
